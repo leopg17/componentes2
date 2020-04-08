6d9261730cfe0559980f4d4c91eaffcb49ddf4bb
Leonardo Porras <leopg17@gmail.com>
2020-04-08 08:19:28 -0600

    Imput Fields

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -5,14 +5,16 @@ import 'package:componentes2/src/pages/alert_page.dart';
 import 'package:componentes2/src/pages/avatar_page.dart';
 import 'package:componentes2/src/pages/home_page.dart';
 import 'package:componentes2/src/pages/animated_container.dart';
+import 'package:componentes2/src/pages/input_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
   return <String, WidgetBuilder>{
-    '/'        : (BuildContext context) => HomePage(),
-    'alert'    : (BuildContext context) => AlertPage(),
-    'avatar'   : (BuildContext context) => AvatarPage(),
-    'card'     : (BuildContext context) => CardPage(),
+    '/'                     : (BuildContext context) => HomePage(),
+    'alert'                 : (BuildContext context) => AlertPage(),
+    'avatar'                : (BuildContext context) => AvatarPage(),
+    'card'                  : (BuildContext context) => CardPage(),
     'animatedContainer'     : (BuildContext context) => AnimatedContainerPage(),
+    'inputs'                 : (BuildContext context) => InputPage(),
   };
 
 }
