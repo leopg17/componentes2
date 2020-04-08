97501e6ef7acc14f03a230161f55bdd1d7f507da
Leonardo Porras <leopg17@gmail.com>
2020-04-07 18:43:53 -0600

    Widget Alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,6 +9,15 @@ class AlertPage extends StatelessWidget {
         title: Text('Alert Page'),
 
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar Alertas'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _mostrarAlert(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.keyboard_backspace),
         onPressed: (){
@@ -16,5 +25,36 @@ class AlertPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void _mostrarAlert(BuildContext context){
+    showDialog(
+      context: context,
+      barrierDismissible: true, //permite cerrar con darle clic afuera
+      builder: (context){
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es el contenido de la caja de alerta'),
+              FlutterLogo( size: 100.0)
+            ]
+          ),
+          actions: <Widget>[
+            FlatButton(
+              onPressed: () => Navigator.of(context).pop(), 
+              //La funcionalidad del boton cancelar es volver a la pantalla anterior
+              child: Text('Cancelar')
+            ),
+            FlatButton(
+              onPressed: (){}, 
+              child: Text('Ok')
+            )
+          ],
+        );
+      }
+      );
   }
 }
