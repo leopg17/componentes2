d3c15d85dc7103e1e27807359ab65f61f7344ecf
Leonardo Porras González <lporras@bolsacr.com>
2020-05-06 14:44:09 -0600

    Listas de Imagenes

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -7,6 +7,7 @@ import 'package:componentes2/src/pages/home_page.dart';
 import 'package:componentes2/src/pages/animated_container.dart';
 import 'package:componentes2/src/pages/input_page.dart';
 import 'package:componentes2/src/pages/slider_page.dart';
+import 'package:componentes2/src/pages/listview_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
   return <String, WidgetBuilder>{
@@ -16,7 +17,8 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
     'card'                  : (BuildContext context) => CardPage(),
     'animatedContainer'     : (BuildContext context) => AnimatedContainerPage(),
     'inputs'                : (BuildContext context) => InputPage(),
-    'slider'                : (BuildContext context) => SliderPage()
+    'slider'                : (BuildContext context) => SliderPage(),
+    'list'                  : (BuildContext context) => ListaPage()
   };
 
 }
